714d35435052bf9e2743ee4676143ec2540c3a34
Einstrasse <hg9587@naver.com>
2020-07-28 01:52:04 +0900

    unstable static hello world server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::{Write};
 
-fn handle_client(stream: TcpStream) {
-    println!("Connection!")
+fn handle_client(mut stream: TcpStream) {
+    println!("Connection!");
+    let data = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 14\r\n\r\nHello world!\r\n";
+    stream.write(data.as_bytes()).expect("Response error");
+    
 }
 
 fn main() -> std::io::Result<()> {
