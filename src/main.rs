1e899d2c0a5e022f9adc6254a870f189a08240f7
hangil-jung <hangil.jung@samsung.com>
2020-07-29 19:20:35 +0900

    flush after send response. fixed about google chrome bug

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::io::{Write};
 use std::io::prelude::*;
 
 fn handle_client(mut stream: TcpStream) {
-    println!("Connection!");
-    let mut buf = [0; 500];
-    let data = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 14\r\n\r\nHello world!\r\n";
-    // stream.read(&buf).expect("Receive error");
-    stream.read(&mut buf).expect("Receive error");
-    println!("Received HTTP Request...\n{}", String::from_utf8_lossy(&buf[..]));
-    stream.write(data.as_bytes()).expect("Response error");
+    //get request and dump
+    let mut request = [0; 1024];
+    stream.read(&mut request).unwrap();
+    println!("Received HTTP Request...\n{}", String::from_utf8_lossy(&request[..]));
+
+    //send hello world response
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 14\r\n\r\nHello world!\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
     
 }
 
