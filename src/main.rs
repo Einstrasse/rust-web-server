1ec612e362c314f1a6f7431063694c14bc335136
hangil-jung <hangil.jung@samsung.com>
2020-07-29 19:31:03 +0900

    handle request check whether request endpoint is /

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ fn handle_client(mut stream: TcpStream) {
     let mut request = [0; 1024];
     stream.read(&mut request).unwrap();
     println!("Received HTTP Request...\n{}", String::from_utf8_lossy(&request[..]));
-
-    //send hello world response
-    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 14\r\n\r\nHello world!\r\n";
+    let response;
+    if request.starts_with(b"GET / HTTP/1.1\r\n") {
+        //send hello world response
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 14\r\n\r\nHello world!\r\n";
+    } else {
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\nConnection: close\r\nContent-Length: 4\r\n\r\nNope\r\n";
+    }
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-    
+
 }
 
 fn main() -> std::io::Result<()> {
